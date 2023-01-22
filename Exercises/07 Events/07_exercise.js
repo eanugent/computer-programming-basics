a460e59c78ea377a7fd7b1cc2236df39d1026e86
eanugent <ean.nugent@gmail.com>
2023-01-22 11:41:49 -0500

    Reorganize Exercise 7 and add missing instruction to Exercise 4

---
+++ Exercises/07 Events/07_exercise.js
@@ -0,0 +1,80 @@
+let avatar = {
+  x: 200,
+  y: 50,
+  color: 'gray'
+}
+let blocks
+
+function moveAvatarLeft(spaces){
+  moveAvatar('x', -1 * spaces)
+}
+
+function moveAvatarRight(spaces){
+  moveAvatar('x', spaces)
+}
+
+function moveAvatarUp(spaces){
+  moveAvatar('y', -1 * spaces)
+}
+
+function moveAvatarDown(spaces){
+  moveAvatar('y', spaces)
+}
+
+function changeAvatarColor(color){
+  avatar.color = color
+  drawAvatar()
+}
+
+function setAvatarX(newX){
+  avatar.x = newX
+}
+
+function setAvatarY(newY){
+  avatar.y = newY
+}
+
+function getAvatarX(){
+  return avatar.x
+}
+
+function getAvatarY(){
+  return avatar.y
+}
+
+function getAvatarColor(){
+  return avatar.color
+}
+
+function drawTargetBlocks(){
+  blocks = []
+  blocks.push({ x: 50, y: 50 })
+  blocks.push({ x: 150, y: 150 })
+  blocks.push({ x: 200, y: 250 })
+
+  drawBlocks(blocks)
+}
+
+function moveAvatar(axis, spaces){
+  clearAvatar()
+
+  if(axis === 'x'){
+    let newX = avatar.x + spaces
+    setAvatarX(newX)
+  }
+  else if(axis === 'y'){
+    let newY = (avatar.y + spaces)
+    setAvatarY(newY)
+  }
+  else {
+    throw 'axis is an invalid value'
+  }
+
+  let hitBlockIndex = indexOfBlockHitByAvatar()
+  if(hitBlockIndex > -1){
+    clearBlock(hitBlockIndex)
+    playSound('beep.wav')
+  }
+
+  drawAvatar()
+}
--- Exercises/07 Events/07_support.js
+++ Exercises/07 Events/07_support.js
@@ -1,17 +1,9 @@
 let context
-let running = false
 const blockWidth = 50
 const blockHeight = 50
 const localFilePath = './'
 const remoteFilePath = 'https://cdn.jsdelivr.net/gh/eanugent/computer-programming-basics@v0.1.9/assets/'
 const soundFilePath = remoteFilePath // Change this to localFilePath to use your own files
-
-function initialize(){
-  let canvas = document.querySelector('#playarea')
-  canvas.setAttribute('width', '450')
-  canvas.setAttribute('height', '450')
-  context = canvas.getContext('2d')
-}
 
 function clearBlock(blockIndex){
   context.clearRect(blocks[blockIndex].x, blocks[blockIndex].y, blockWidth, blockHeight)
@@ -46,8 +38,9 @@ function blockHitAvatar(block){
   return xInRange && yInRange
 }
 
+
 function playSound(filename){
-  var audio = new Audio(soundFilePath + filename);
+  var audio = new Audio(filename);
   audio.play();
 }
 
@@ -68,20 +61,13 @@ function drawAvatar(){
   )
 }
 
-function sleep(ms){
-  return new Promise(resolve => setTimeout(resolve, ms))  
-}
-
 window.onload = async function(){
+  let canvas = document.querySelector('#playarea')
+  canvas.setAttribute('width', '450')
+  canvas.setAttribute('height', '450')
+  context = canvas.getContext('2d')
+
   initialize()
-  document.addEventListener('keydown', async (e) =>{
-    if(running) return
-    if(e.key == 'Enter'){
-      running = true
-      context.clearRect(0,0,450,450)
-      if(typeof blocks !== 'undefined') blocks.length = 0
-      await run()
-      running = false
-    }
-  })
+  drawAvatar()
+  drawTargetBlocks()
 }
